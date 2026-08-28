56ad4020b7f4be24b4e517d5001f5c2cc70c1f9b
Mayck Mitchell C. Da Silva <mayckmitchell15@gmail.com>
2026-08-27 22:21:36 -0300

    Create Main.java

---
+++ Aulas/main.java
@@ -0,0 +1,25 @@
+public class Main {
+    //Variaveis são espaços no programa aonde podemos agregar valores exemplos:
+
+        public static void main(String[] args) {
+            tipo nomedaVariavel = valor;
+
+            int minhaidade = 20;
+            String meunomme = "Mitchell";
+
+        // Numeros Inteiros = 200,9019209, 133333, 21898...
+            byte 8 bits => -128 a 127
+                byte minhaVar = 255; erro
+
+            short 16 bits => -32.768 a 32.767
+                short minhaVar = 100.000; erro
+
+            int => 32 bits
+
+            long => 64 bits
+
+        //Numeros Decimais = 1.2, 88.90, 90.0....
+            float  => 32 bits
+            double => 64 bits
+
+        }
